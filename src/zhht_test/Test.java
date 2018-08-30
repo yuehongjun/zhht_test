a570878155ed57332787ceaec92d8dc1341d5ae5
hongjun.yue <hongjun.yue@asiainnovations.com>
2018-08-30 13:59:09 -0700

    ok

--- src/zhht_test/Test.java
+++ src/zhht_test/Test.java
@@ -5,7 +5,10 @@ public class Test {
 	public Test() {
 		// TODO Auto-generated constructor stub
 	}
-
+/**
+* 主函数使用调用
+*
+*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
